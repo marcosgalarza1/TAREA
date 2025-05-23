48c8aa9a11d76237635180e4a6f821306213dfd8
marcosgalarza1 <luismarcogalarza10@gmail.com>
2025-05-23 13:14:30 -0400

    Agregar clase Nodo con constructor y atributos iniciales para implementación futura de un árbol AVL.

--- python.py
+++ python.py
@@ -1,1 +1,6 @@
-
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierda = None
+        self.derecha = None
+        self.altura = 1 # Para usos futuros en AVL
